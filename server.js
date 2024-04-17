800ef732f8f7d5aa1674c7fabd3fa62996284346
DivyanshuKaanaujia <124723859+DivyanshuKaanaujia@users.noreply.github.com>
2024-04-17 22:15:15 +0530

    added functionality to run codes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const http = require('http')
+const http = require('http');
+const path = require('path');
 const {Server} = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
@@ -7,6 +8,11 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server)
+
+app.use(express.static('build'))
+app.use((req,res,next)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
 
 const userSocketMap = {}
 
--- src/components/editor.js
+++ src/components/editor.js
@@ -1,31 +1,68 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/dracula.css';
 import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/clike/clike';
 import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
 import ACTIONS from '../Actions';
+import axios from 'axios'
 
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
     const editorRef = useRef(null);
+    const [output, setOutput] = useState('');
+    const [selectedLanguage, setSelectedLanguage] = useState('javascript');
+
+    async function runcpp(code){
+
+        const options = {
+        method: 'POST',
+        url: 'https://online-code-compiler.p.rapidapi.com/v1/',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '09c66274c4msh5d6a443d46f7ecbp130abcjsn53e5c3e75a18',
+            'X-RapidAPI-Host': 'online-code-compiler.p.rapidapi.com'
+        },
+        data: {
+            language: 'cpp17',
+            version: 'latest',
+            code: code,
+            input: null
+        }
+        };
+
+        try {
+            const response = await axios.request(options);
+            console.log(response.data);
+            return response.data
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     useEffect(() => {
         async function init() {
+            if (editorRef.current) {
+                editorRef.current.toTextArea();
+                editorRef.current = null;
+            }
+        
             editorRef.current = Codemirror.fromTextArea(
                 document.getElementById('realtimeEditor'),
                 {
-                    mode: { name: 'javascript', json: true },
+                    mode: { name: selectedLanguage },
                     theme: 'dracula',
                     autoCloseTags: true,
                     autoCloseBrackets: true,
                     lineNumbers: true,
                 }
             );
-
+        
             editorRef.current.on('change', (instance, changes) => {
                 const { origin } = changes;
                 const code = instance.getValue();
-                onCodeChange(code)
+                onCodeChange(code);
                 if (origin !== 'setValue') {
                     socketRef.current.emit(ACTIONS.CODE_CHANGE, {
                         roomId,
@@ -34,8 +71,9 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
                 }
             });
         }
+        
         init();
-    }, []);
+    }, [selectedLanguage]);
 
     useEffect(() => {
         if (socketRef.current) {
@@ -51,7 +89,41 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
         };
     }, [socketRef.current]);
 
-    return <textarea id="realtimeEditor"></textarea>;
+    const handleRun = async () => {
+        try {
+            const code = editorRef.current.getValue();
+            let result;
+            if (selectedLanguage === 'javascript') {
+                result = eval(code);
+            } else {
+                const response = await runcpp(code);
+                result = `CPU Time: ${response.cpuTime}ms, Memory: ${response.memory} bytes, Output: ${response.output}`;
+            }
+            setOutput(result);
+        } catch (error) {
+            setOutput(error.toString());
+        }
+    };
+    
+    
+
+    const handleLanguageChange = (event) => {
+        setSelectedLanguage(event.target.value);
+    };
+
+    return (
+        <div>
+            <select value={selectedLanguage} onChange={handleLanguageChange}>
+                <option value="javascript">JavaScript</option>
+                <option value="text/x-c++src">C++</option>
+            </select>
+            <br />
+            <textarea id="realtimeEditor"></textarea>
+            <br />
+            <button onClick={handleRun}>Run</button>
+            <div>{output}</div>
+        </div>
+    );
 };
 
 export default Editor;
